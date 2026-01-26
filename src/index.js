7f1ac0a2d92ed7d50415e28d16c0d8bd72468c26
Himanshu Ojha <ojha33388@gmail.com>
2026-01-27 01:52:03 +0530

    connect mongodb to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// require('dotenv').config({path : './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+////second method to connect mongodb to local server
+
+// import express from "express";
+// const app = express()
+
+// ( async () => {
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error" ,(error) =>{
+//         console.log("ERROR :" , error);
+//         throw error
+//        } )
+
+//        app.listen(process.nextTick.PORT , () => {
+//         console.log(`App is listening on port ${process.env.PORT}`);
+        
+//        })
+//     } catch (error) {
+//         console.error("ERROR",error)
+//         throw err
+//     }
+// })()
