6fc2e6e6f3fd8a8eacefa7768a3c08ff56ae1bd6
Himanshu Ojha <ojha33388@gmail.com>
2026-01-31 03:15:19 +0530

    models was created and learn new mongoose

--- src/db/index.js
+++ src/db/index.js
@@ -5,10 +5,9 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
        const connectInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST : ${connectInstance.connection.host}`);
-        
+        console.log(`\n MongoDB connected !! DB HOST : ${connectInstance.connection.host}`);        
     } catch (error) {
-        console.log("MONGODB connection error" , error);
+        console.log("MONGODB connection failed" , error);
         process.exit(1)
     }
 }
